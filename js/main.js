6c77961814b3e99783ca00aa97145c66be484753
Nick <nick@nickstellato.com>
2016-06-03 02:30:05 +0000

    started styling homepage

--- js/main.js
+++ js/main.js
@@ -4,10 +4,8 @@ var gallery = document.getElementById("photo-gallery");
 var output = '';
 
 for (var i = 0; i < photos.length; i++) {
-    output += "<a href=." + photos[i].imageURL + ">"
-    output += "<figure>";
+    output += "<a class='photo' href=." + photos[i].imageURL + ">"
     output += "<img src=." + photos[i].thumbnailURL + "></img>";
-    output += "</figure>";
     output += "</a>"
 }
 
