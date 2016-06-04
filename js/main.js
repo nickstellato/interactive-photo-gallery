b6d785cabfaab44974ed0d10dfe4e472b124a04d
Nick <nick@nickstellato.com>
2016-06-04 02:58:24 +0000

    finished layout of homepage

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ var gallery = document.getElementById("photo-gallery");
 var output = '';
 
 for (var i = 0; i < photos.length; i++) {
-    output += "<a class='photo' href=." + photos[i].imageURL + ">"
+    output += "<figure class='photo'>"
+    output += "<a href=." + photos[i].imageURL + ">"
     output += "<img src=." + photos[i].thumbnailURL + "></img>";
     output += "</a>"
+    output += "</figure>"
 }
 
 gallery.innerHTML = output;
