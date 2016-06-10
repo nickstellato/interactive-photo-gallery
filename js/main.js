0ee0eea5ebbfacb56487ad13314c7f2eb29cb202
Nick <nick@nickstellato.com>
2016-06-10 01:33:52 +0000

    created self ivoking function

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
-/* global photos */
+/* global photos, jQuery */
 
-var gallery = document.getElementById("photo-gallery");
-var output = '';
+(function($){
+    
+    var gallery = document.getElementById("photo-gallery");
+    var photo = document.getElementsByClassName(photo);
+    var output = '';
+    
+    for (var i = 0; i < photos.length; i++) {
+        output += "<figure class='photo'>";
+        output += "<a href=./img/photos/" + photos[i].name + ">";
+        output += "<img src=./img/thumbnails/" + photos[i].name + "></img>";
+        output += "</a>";
+        output += "</figure>";
+    }
+    
+    gallery.innerHTML = output;
+    
+    $("figure").click(function(event) {
+        event.preventDefault();
 
-for (var i = 0; i < photos.length; i++) {
-    output += "<figure class='photo'>";
-    output += "<a href=./img/photos/" + photos[i].name + ">";
-    output += "<img src=./img/thumbnails/" + photos[i].name + "></img>";
-    output += "</a>";
-    output += "</figure>";
-}
-
-gallery.innerHTML = output;
+    });
+    
+    
+})(jQuery);
