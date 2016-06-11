49896eb1726bed348480ef11498716212644dbc5
Nick Stellato <nick@nickstellato.com>
2016-06-11 07:25:21 -0500

    Lightbox displays the appropriate clicked picture

--- js/main.js
+++ js/main.js
@@ -7,36 +7,37 @@
     var output = '';
 
     for (var i = 0; i < photos.length; i++) {
-        output += "<figure class='photo'>";
-        output += "<a href=./img/photos/" + photos[i].name + ">";
-        output += "<img src=./img/thumbnails/" + photos[i].name + "></img>";
-        output += "</a>";
-        output += "</figure>";
+      output += "<figure class='photo'>";
+      output += "<a href=./img/photos/" + photos[i].name + ">";
+      output += "<img src=./img/thumbnails/" + photos[i].name + "></img>";
+      output += "</a>";
+      output += "</figure>";
     }
 
     $gallery.append(output);
 
-    // var clickedPhotoURL =
-
-    var lightboxPhoto  = "<figure id='lightboxPhoto'>";
-        lightboxPhoto += "<img src='./img/photos/01.jpg'></img>";
-        lightboxPhoto += "<figcaption>Hello World</figcaption>";
-        lightboxPhoto += "</figure>";
-
     $("figure").on("click", function(e){
-        e.preventDefault();
+      e.preventDefault();
 
-        if (!$("#overlay").length) {
-          $(".container").prepend("<div id='overlay'></div>");
-        }
+      if (!$("#overlay").length) {
+        $(".container").prepend("<div id='overlay'></div>");
+      }
 
-        $("#overlay").addClass("overlay");
-        $("#overlay").prepend(lightboxPhoto);
+      $("#overlay").addClass("overlay");
+
+      var photoURL = ($(this).children('a')).attr("href");
+
+      var lightboxPhoto  = "<figure id='lightboxPhoto'>";
+          lightboxPhoto += "<img src=" + photoURL + "></img>";
+          lightboxPhoto += "<figcaption>Hello World</figcaption>";
+          lightboxPhoto += "</figure>";
+
+      $("#overlay").prepend(lightboxPhoto);
     });
 
     $("#overlay").on("click", function(){
-       $("#lightboxPhoto").remove();
-       $("#overlay").removeClass("overlay");
+      $("#lightboxPhoto").remove();
+      $("#overlay").removeClass("overlay");
     });
 
 })(jQuery);
