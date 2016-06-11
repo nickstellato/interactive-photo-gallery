3c4aaff62604eb64c9cadbd3fcd1b5ef49deb39a
Nick Stellato <nick@nickstellato.com>
2016-06-11 07:54:37 -0500

    Continue working on positioning navigation arrows in the lightbox

--- js/main.js
+++ js/main.js
@@ -27,15 +27,18 @@
 
       var photoURL = ($(this).children('a')).attr("href");
 
-      var lightboxPhoto  = "<figure id='lightboxPhoto'>";
+      var lightboxPhoto  = "<i class='fa fa-angle-left fa-pull-left fa-5x'></i>";
+          lightboxPhoto += "<figure id='lightboxPhoto'>";
           lightboxPhoto += "<img src=" + photoURL + "></img>";
           lightboxPhoto += "<figcaption>Hello World</figcaption>";
           lightboxPhoto += "</figure>";
+          lightboxPhoto += "<i class='fa fa-angle-right fa-pull-right fa-5x'></i>";
 
       $("#overlay").prepend(lightboxPhoto);
     });
 
     $("#overlay").on("click", function(){
+      $("i").remove();
       $("#lightboxPhoto").remove();
       $("#overlay").removeClass("overlay");
     });
