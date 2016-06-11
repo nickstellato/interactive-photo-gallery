cccfd898d30185309ec76c82e815d86e77c933d3
Nick Stellato <nick@nickstellato.com>
2016-06-11 06:46:49 -0500

    Refactored opening and closing the lightbox

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 (function($){
     'use strict';
-    
+
     var $gallery = $("#photo-gallery");
     var output = '';
-    
+
     for (var i = 0; i < photos.length; i++) {
         output += "<figure class='photo'>";
         output += "<a href=./img/photos/" + photos[i].name + ">";
@@ -13,30 +13,30 @@
         output += "</a>";
         output += "</figure>";
     }
-    
+
     $gallery.append(output);
-    
+
+    // var clickedPhotoURL =
+
     var lightboxPhoto  = "<figure id='lightboxPhoto'>";
         lightboxPhoto += "<img src='./img/photos/01.jpg'></img>";
         lightboxPhoto += "<figcaption>Hello World</figcaption>";
         lightboxPhoto += "</figure>";
-    
+
     $("figure").on("click", function(e){
         e.preventDefault();
-        
-        if ($("#overlay").length){
-            $("#overlay").addClass("overlay");
-            $("#overlay").prepend(lightboxPhoto);
-        } else {
-            $(".container").prepend("<div id='overlay'></div>");
-            $("#overlay").addClass("overlay");
-            $("#overlay").prepend(lightboxPhoto);
+
+        if (!$("#overlay").length) {
+          $(".container").prepend("<div id='overlay'></div>");
         }
+
+        $("#overlay").addClass("overlay");
+        $("#overlay").prepend(lightboxPhoto);
     });
-    
+
     $("#overlay").on("click", function(){
        $("#lightboxPhoto").remove();
        $("#overlay").removeClass("overlay");
     });
-    
+
 })(jQuery);
