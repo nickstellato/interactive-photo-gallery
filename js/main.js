367cb55900412b1369d9883161c2f6e013a4f7dc
Nick <nick@nickstellato.com>
2016-06-11 03:11:41 +0000

    positioned image and figure caption in lightbox

--- js/main.js
+++ js/main.js
@@ -16,25 +16,16 @@
     
     $gallery.append(output);
     
-    //Need to add the output that is added to the lightbox
-    //What do we need:
-        //Left Arrow that clicks to the previous picture
-        //Image
-        //Figure caption
-        //Right Arrow that moves forward when clicked
+    var lightboxPhoto  = "<div id='overlay' class='overlay'>"
+        lightboxPhoto += "<figure id='lightboxPhoto'>";
+        lightboxPhoto += "<img src='./img/photos/01.jpg'></img>";
+        lightboxPhoto += "<figcaption>Hello World</figcaption>";
+        lightboxPhoto += "</figure>";
+        lightboxPhoto += "</div>"
     
     $("figure").on("click", function(e){
         e.preventDefault();
-        $("#lightbox-container").addClass("overlay");
-        var lightboxPhoto  = "<figure>";
-            lightboxPhoto += "<img src=" + $(this).find('a').attr("href") + "></img>";
-            lightboxPhoto += "<figcaption>" + photos[0].caption + "</figcaption>";
-            lightboxPhoto += "</figure>";
-        $("#lightbox").html(lightboxPhoto);
-    });
-    
-    $("#lightbox-container").on("click", function(){
-        $(this).removeClass("overlay");
+        $(".container").prepend(lightboxPhoto);
     });
     
 })(jQuery);
