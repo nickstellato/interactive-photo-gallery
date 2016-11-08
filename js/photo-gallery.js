aa23666a10c8f9f2d17ab0852f372a36f02aaf07
Nick Stellato <nick@nickstellato.com>
2016-11-08 12:43:31 -0600

    working on adding figure to lightbox when clicked

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,14 +1,20 @@
 (function(){
     var $photos = document.querySelectorAll("img");
     var $lightbox = document.getElementById("lightbox");
+    var $figure = document.createElement("figure");
+    var $largeImage = document.createElement("img");
 
     $photos.forEach(function(photo){
-        photo.addEventListener("click", function(){
-            $lightbox.className = "lightbox";
-        });
+        photo.addEventListener("click", illuminate, false);
     });
 
-    $lightbox.addEventListener("click", function(){
+    $lightbox.addEventListener("click", removeLightbox, false);
+
+    function illuminate(){
+        $lightbox.className = "lightbox";
+    }
+
+    function removeLightbox(){
         $lightbox.removeAttribute("class");
-    });
+    }
 })();
