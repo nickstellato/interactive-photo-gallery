501a698b12df79a40603bfc921ff995194a24915
Nick Stellato <nick@nickstellato.com>
2016-11-07 21:52:41 -0600

    still working on JS

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,13 +1,3 @@
 (function(){
-    var $photos = document.getElementsByClassName("photo");
-    var $body = document.getElementsByTagName("body");
-
-    //when you click on a photo, the photo adds the overlay class to the body
-
-    for (var i = 0; i < $photos.length(); i++){
-        $photos[i].addEventListener("click", function(){
-            $body.className += "overlay";
-        })
-
-    }
+    
 })();
