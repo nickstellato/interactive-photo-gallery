ea013814cbc70c1574ede1488ba5236691994fab
Nick Stellato <nick@nickstellato.com>
2016-11-09 18:49:59 -0600

    still messing with the click functions

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,6 +1,7 @@
 (function(){
+    "use strict";
+
     var $photos = getPhotosArray();
-    console.log($photos);
     var $lightbox = document.getElementById("lightbox");
 
     $photos.forEach(function(photo){
@@ -9,28 +10,27 @@
 
     function illuminate(){
         $lightbox.setAttribute("class", "lightbox");
-        $lightbox.addEventListener("click", removeLightbox, false);
 
-        $photoURL = this.getAttribute("src").replace("/thumbnails", "");
-        $img = document.createElement("img");
+        var $photoURL = this.getAttribute("src").replace("/thumbnails", "");
+        var $img = document.createElement("img");
         $img.setAttribute("src", $photoURL);
         $img.setAttribute("class", "largePhoto");
         $img.setAttribute("id", "currentPhoto");
 
-        $caption = this.getAttribute("alt");        
-        $figcaption = document.createElement("figcaption");
+        var $caption = this.getAttribute("alt");        
+        var $figcaption = document.createElement("figcaption");
         $figcaption.innerText = $caption;
         $figcaption.setAttribute("class", "caption");
 
-        $figure = document.createElement("figure");
+        var $figure = document.createElement("figure");
 
-        $leftArrow = document.createElement("a");
+        var $leftArrow = document.createElement("a");
         $leftArrow.setAttribute("class", "arrow arrow-left");
         $leftArrow.setAttribute("id", "previous");
         $leftArrow.innerHTML = '&#10094;';
-        $leftArrow.addEventListener("click", getPreviousPhoto, false);
+        $leftArrow.addEventListener("click", function(){}, false);
 
-        $rightArrow = document.createElement("a");
+        var $rightArrow = document.createElement("a");
         $rightArrow.setAttribute("class", "arrow arrow-right");
         $rightArrow.setAttribute("id", "next");        
         $rightArrow.innerHTML = '&#10095;';
@@ -41,34 +41,20 @@
         $figure.appendChild($rightArrow);
         $figure.appendChild($figcaption);
         $lightbox.appendChild($figure);
+    }
 
-        function getPreviousPhoto(){}
-
-        function getNextPhoto(){}
-
-        // currentPhoto = document.getElementById("currentPhoto");
-        // console.log(currentPhoto);
-        // currentPhotoSRC = currentPhoto.getAttribute("src")
-        // console.log(currentPhotoSRC);
-        // currentPosition = $photos.indexOf(currentPhotoSRC);
-        // console.log(currentPosition);
-
-        currentPhoto = $photos.indexOf(this);
-        nextPhoto = currentPhoto + 1;
-        previousPhoto = currentPhoto - 1;
-        console.log(previousPhoto);
-        console.log(nextPhoto);
-
-        console.log($photos.indexOf(this));
+    function getNextPhoto(){
+        var currentPhotoIndex = $photos.indexOf(this);
+        var nextPhotoIndex = currentPhotoIndex + 1;
+        var nextPhotoSRC = $photos[nextPhotoIndex].getAttribute("src");
+        var nextPhotoALT = $photos[nextPhotoIndex].getAttribute("alt");
+        var currentPhoto = document.getElementById("currentPhoto");
+        currentPhoto.setAttribute("src", nextPhotoSRC);
+        currentPhoto.setAttribute("alt", nextPhotoALT);
     }
 
     function getPhotosArray(){
-        $photosList = document.querySelectorAll("img");
+        var $photosList = document.querySelectorAll("img");
         return Array.prototype.slice.call($photosList);
-    }
-
-    function removeLightbox(){
-        $lightbox.removeAttribute("class");
-        $lightbox.removeChild($figure);
     }
 })();
