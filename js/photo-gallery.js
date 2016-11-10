b4a8dd88f93f823e034171fcfb03b523321c225f
Nick Stellato <nick@nickstellato.com>
2016-11-10 16:30:07 -0600

    added makeElement function to expedite creating elements with attributeds

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -1,93 +1,92 @@
 (function(){
     "use strict";
 
-    var $photos = getPhotosArray();
-    console.log($photos);
-    var $lightbox = document.getElementById("lightbox");
+    var $photos = makeArray("img")
 
     $photos.forEach(function(photo){
         photo.addEventListener("click", illuminate, false);
     });
 
     function illuminate(){
+        var $lightbox = document.getElementById("lightbox");
         $lightbox.setAttribute("class", "lightbox");
+        
         var _this = this;
-
         var $img = makeImage(_this);
-
-        
-
-        
-
-        var $caption = this.getAttribute("alt");        
-        var $figcaption = document.createElement("figcaption");
-        $figcaption.innerText = $caption;
-        $figcaption.setAttribute("class", "caption");
-
-        var $figure = document.createElement("figure");
-
-        var $leftArrow = document.createElement("a");
-        $leftArrow.setAttribute("class", "arrow arrow-left");
-        $leftArrow.setAttribute("id", "previous");
-        $leftArrow.innerHTML = '&#10094;';
-        $leftArrow.addEventListener("click", function(){}, false);
-
-        var $rightArrow = document.createElement("a");
-        $rightArrow.setAttribute("class", "arrow arrow-right");
-        $rightArrow.setAttribute("id", "next");        
-        $rightArrow.innerHTML = '&#10095;';
-        $rightArrow.addEventListener("click", function(){
-            var currentPhotoIndex = $photos.indexOf(_this);
-            var nextPhotoIndex = currentPhotoIndex + 1;
-            var nextPhoto = $photos[nextPhotoIndex];
-            console.log(nextPhoto);
-            $caption = nextPhoto.getAttribute("alt");
-            $img = nextPhoto.getAttribute("src");
-        }, false);
-
-        $figure.appendChild($leftArrow);
-        $figure.appendChild($img);
-        $figure.appendChild($figcaption);
-        $figure.appendChild($rightArrow);
+        var $figcaption = makeFigCaption(_this);
+        var $leftArrow = makeArrow(_this, "left", $figcaption, $img);
+        var $rightArrow = makeArrow(_this, "right", $figcaption, $img);
+        var $figure = makeFigure($img, $figcaption, $leftArrow, $rightArrow);
         $lightbox.appendChild($figure);
-
-        // var currentPhotoIndex = $photos.indexOf(this);
-        // console.log(currentPhotoIndex);
-        // var nextPhotoIndex = incrementIndex(currentPhotoIndex);
-        // console.log(nextPhotoIndex);
-        // var prevPhotoIndex = decrementIndex(currentPhotoIndex);
-
-        // function setNextPhoto(index) {
-        //     var nextPhoto = $photos[index];
-        //     console.log(nextPhoto);
-        //     var nextPhotoSRC = nextPhoto.values("src");
-        //     var nextPhotoALT = nextPhoto.values("alt");
-        //     $img.setAttribute("src", nextPhotoSRC);
-        //     $img.setAttribute("alt", nextPhotALT);
-        // }
-
-        // function incrementIndex(index) {
-        //     return index + 1;
-
-        // }
-
-        // function decrementIndex(index) {
-        //     return index - 1;
-        // }
     }
 
     function makeImage(el){
-        var $photoURL = el.getAttribute("src")
-            .replace("/thumbnails", "");
         var $img = document.createElement("img");
-        $img.setAttribute("src", $photoURL);
         $img.setAttribute("class", "largePhoto");
         $img.setAttribute("id", "currentPhoto");
+        var $photoURL = el.getAttribute("src")
+            .replace("/thumbnails", "");
+        $img.setAttribute("src", $photoURL);
         return $img;
     }
 
-    function getPhotosArray(){
-        var $photosList = document.querySelectorAll("img");
-        return Array.prototype.slice.call($photosList);
+    function makeFigCaption(el){
+        var $caption = el.getAttribute("alt");        
+        var $figcaption = document.createElement("figcaption");
+        $figcaption.innerText = $caption;
+        $figcaption.setAttribute("class", "caption");
+        return $figcaption;
+    }
+
+    function makeArrow(el, direction, caption, img){
+        var arrow = document.createElement("a");
+        if (direction === "left"){
+            arrow.setAttribute("id", "previous");
+            arrow.setAttribute("class", "arrow arrow-left");
+            arrow.innerHTML = '&#10094;';
+            arrow.addEventListener("click", function(){}, false);
+        } else if (direction === "right"){
+            arrow.setAttribute("id", "next");
+            arrow.setAttribute("class", "arrow arrow-right");
+            arrow.innerHTML = '&#10095;';
+            arrow.addEventListener("click", function(){
+                var currentPhotoIndex = $photos.indexOf(el);
+                var nextPhotoIndex = currentPhotoIndex + 1;
+                var nextPhoto = $photos[nextPhotoIndex];
+                console.log(nextPhoto);
+                caption = nextPhoto.getAttribute("alt");
+                img = nextPhoto.getAttribute("src");
+            }, false);
+        } else {
+            return Error("Need a direction!");
+        }
+
+        return arrow
+    }
+
+    function makeFigure(img, figcaption, leftArrow, rightArrow) {
+        var $figure = document.createElement("figure");
+        $figure.appendChild(leftArrow);
+        $figure.appendChild(img);
+        $figure.appendChild(figcaption);
+        $figure.appendChild(rightArrow);
+        return $figure;
+    }
+
+    function makeElement(tag, classes, ids){
+        var element = document.createElement(tag);
+        element.setAttribute("id", ids);
+
+        if (typeof classes === "string"){
+            element.setAttribute("class", classes);
+        } else {
+            element.setAttribute("class", classes.join(" "));
+        }
+
+        return element;
+    }
+
+    function makeArray(tag){
+        return Array.prototype.slice.call(document.querySelectorAll(tag));
     }
 })();
