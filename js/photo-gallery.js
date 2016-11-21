40133dfa59fc37fc3c7ff83926b67c609f8f9f6c
Nick Stellato <nick@nickstellato.com>
2016-11-21 12:34:41 -0600

    added close button and it works!

--- js/photo-gallery.js
+++ js/photo-gallery.js
@@ -13,6 +13,7 @@
     function illuminate(){
         var index = photos.indexOf(this);
         var lightbox = document.getElementById("lightbox");
+        var closeButton = document.getElementById("close");
         var figure = document.getElementById("figure");
         var rightArrow = document.getElementById("next");
         var leftArrow = document.getElementById("previous");
@@ -20,11 +21,21 @@
         var currentCaption = document.getElementById("currentCaption");
             figure.setAttribute("class", "");
             lightbox.setAttribute("class", "lightbox");
+            closeButton.setAttribute("class", "close");
             rightArrow.setAttribute("class", "arrow arrow-right");
             leftArrow.setAttribute("class", "arrow arrow-left");
             currentPhoto.setAttribute("src", photos[index].src.replace("/thumbnails", ""));
             currentCaption.innerText = photos[index].alt;
-            rightArrow.onclick = function(){
+
+            closeButton.addEventListener("click", function(){
+                figure.setAttribute("class", "hidden");
+                lightbox.setAttribute("class", "hidden");
+                closeButton.setAttribute("class", "hidden");
+                rightArrow.setAttribute("class", "hidden");
+                leftArrow.setAttribute("class", "hidden");
+            }, false)
+
+            rightArrow.addEventListener("click", function(){
                 if (index === 11) {
                     index = 0;
                 } else {
@@ -33,8 +44,9 @@
                 currentPhoto.setAttribute("src", photos[index].src
                     .replace("/thumbnails", ""));
                 currentCaption.innerText = photos[index].alt;
-            }
-            leftArrow.onclick = function(){
+            }, false);
+            
+            leftArrow.addEventListener("click", function(){
                 if (index === 0) {
                     index = 11;
                 } else {
@@ -43,7 +55,7 @@
                 currentPhoto.setAttribute("src", photos[index].src
                     .replace("/thumbnails", ""));
                 currentCaption.innerText = photos[index].alt;
-            }
+            }, false);
 
     }
 
